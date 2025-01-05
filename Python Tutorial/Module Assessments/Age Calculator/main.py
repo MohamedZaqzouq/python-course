30108575f1ed52d0e61240b7908cf8f8c038fc3e
MohamedZaqzouq <ma485452@gmail.com>
2025-01-05 17:51:25 +0200

    age calculator

---
+++ Python Tutorial/Module Assessments/Age Calculator/main.py
@@ -0,0 +1,4 @@
+start_year=input("Enter the year you were born: ") 
+end_year=input("Enter the year you want to know your age in: ")
+age=int(end_year)-int(start_year)
+print("You will be "+str(age)+" years old in "+end_year)
