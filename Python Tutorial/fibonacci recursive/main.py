5373c9654091d514f584be14e585802395a978c9
MohamedZaqzouq <ma485452@gmail.com>
2025-01-05 23:58:17 +0200

    fibb recursive

---
+++ Python Tutorial/fibonacci recursive/main.py
@@ -0,0 +1,11 @@
+def fibonaci(n):
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+    else:
+        return fibonaci(n-1)+fibonaci(n-2)
+def main():
+    n=int(input("Enter the number of Fibonacci numbers to generate: "))
+    print(f"Fibonacci sequence up to {n} numbers: {fibonaci(n)}")
+main()
